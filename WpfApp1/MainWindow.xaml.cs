ddaf7c84fe5a77c9ac9f78b5e99eb3312fcfbd8f
Jamagiae Medley <jamagiae.medley@outlook.com>
2020-11-12 13:27:03 -0500

    BMI

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,25 +14,61 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
+    [XmlRoot("BMI Calc", Namespace = "www.bmicalc.ninja")]
     public partial class MainWindow : Window
     {
+       
         double h;
         double w;
         double t;
+        public string FilePath = "C:\\Users\\medley_jamagiae\\Documents\\";
+        public string FileName = "Ninja.xml";
         
         public MainWindow()
         {
             InitializeComponent();
+            OnLoadStats();
+        }
+        public class Customer
+        {
+            [XmlAttribute("Last Name")]
+            public string lastName { get; set; }
+            [XmlAttribute("First Name")]
+            public string firstName { get; set; }
+            [XmlAttribute("Phone Number")]
+            public string phoneNumber { get; set; }
+            [XmlAttribute("Height")]
+            public int heightInches { get; set; }
+            [XmlAttribute("Weight")]
+            public int weightLbs { get; set; }
+            [XmlAttribute("Customer BMI")]
+            public int custBMI { get; set; }
+            [XmlAttribute("Status")]
+            public string statusTitle { get; set; }
+
+            
+
         }
 
         private void submit_Click(object sender, RoutedEventArgs e)
         {
+            Customer customer1 = new Customer();
+
+            customer1.lastName = LN.Text;
+            customer1.firstName = FN.Text;
+            customer1.phoneNumber = PN.Text;
+            //customer1.heightInches = Convert.ToInt32(height.Text);
+            //customer1.weightLbs = Convert.ToInt32(weight.Text);
+
             h = double.Parse(height.Text);
             w = double.Parse(weight.Text);
             t = (w / h/h) * 703;
@@ -55,6 +93,40 @@ namespace WpfApp1
             {
                 M.Text = "According to CDC.gov BMI CALCUlator you have a Obese.";
             }
+
+            TextWriter writer = new StreamWriter(FilePath + FileName);
+
+            //Actual Part for Serialization
+            XmlSerializer ser = new XmlSerializer(typeof(Customer));
+
+            //get the writer stream and the instance of the class we have... below
+            ser.Serialize(writer, customer1);
+            writer.Close();
+
+          
+        }
+
+
+        private void OnLoadStats()
+        {
+            Customer cust = new Customer();
+
+            XmlSerializer des = new XmlSerializer(typeof(Customer));
+            using (XmlReader reader = XmlReader.Create(FilePath + FileName))
+            {
+                cust = (Customer)des.Deserialize(reader);
+
+                LN.Text = cust.lastName;
+                FN.Text = cust.firstName;
+                PN.Text = cust.phoneNumber;
+                height.Text = Convert.ToString(cust.heightInches);
+                weight.Text = Convert.ToString(cust.weightLbs);
+
+
+            }
+            DataSet xmlData = new DataSet();
+            xmlData.ReadXml(FilePath + FileName, XmlReadMode.Auto);
+            xDataGrid.ItemsSource = xmlData.Tables[0].DefaultView;
         }
 
         private void clear_Click(object sender, RoutedEventArgs e)
@@ -71,6 +143,11 @@ namespace WpfApp1
         private void exit_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void xDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
         }
     }
 }
